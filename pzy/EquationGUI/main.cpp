21124b8478373daae8c857f28f4e266461575742
zypangpang <pangzaiyu77@gmail.com>
2016-03-03 10:37:40 +0800

    解方程模块
    可解一元一次，二次，三次方程，大家可下载测试

---
+++ pzy/EquationGUI/main.cpp
@@ -0,0 +1,11 @@
+#include "equationmainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    EquationMainWindow w;
+    w.show();
+
+    return a.exec();
+}
